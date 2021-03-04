316a516d6c26df6cabbfe1f830125b357352cd1a
Adam O'Neil <adamosoftware@gmail.com>
2021-03-04 08:15:33 -0500

    Add project files.

---
+++ ImgCloudPaste/Services/ImageCloudPaste.cs
@@ -0,0 +1,59 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using StringIdLibrary;
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ImgCloudPaste.Services
+{
+    public class Settings
+    {
+        /// <summary>
+        /// azure storage connection string
+        /// </summary>
+        public string ConnectionString { get; set; }
+
+        /// <summary>
+        /// upload container
+        /// </summary>
+        public string ContainerName { get; set; }
+    }
+
+    public class ImageCloudPaste
+    {
+        private readonly Settings _settings;
+
+        public ImageCloudPaste(Settings settings)
+        {
+            _settings = settings;
+        }
+
+        public async Task<string> UploadAndGetUrlAsync(Image image)
+        {
+            var info = GetFileInfo(image);
+            var name = StringId.New(10, StringIdRanges.Upper | StringIdRanges.Numeric) + info.extension;            
+            var client = new BlobClient(_settings.ConnectionString, _settings.ContainerName, name);
+            
+            using (var ms = new MemoryStream())
+            {
+                image.Save(ms, image.RawFormat);
+                await client.UploadAsync(ms, new BlobHttpHeaders()
+                {
+                    ContentType = info.contentType
+                });
+            }
+
+            return client.Uri.AbsoluteUri;
+        }
+
+        private (string extension, string contentType) GetFileInfo(Image image) =>
+            (image.RawFormat.Equals(ImageFormat.Png)) ? (".png", "image/png") :
+            (image.RawFormat.Equals(ImageFormat.Jpeg)) ? (".jpg", "image/jpg") :
+            (image.RawFormat.Equals(ImageFormat.Gif)) ? (".gif", "image/gif") :
+            (image.RawFormat.Equals(ImageFormat.Bmp)) ? (".bmp", "image/bitmap") :
+            throw new InvalidOperationException($"Unsupported image format: {image.RawFormat}");
+    }
+}
---
+++ ImgCloudPaste/frmMain.Designer.cs
@@ -0,0 +1,144 @@
+﻿
+namespace ImgCloudPaste
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSettings = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 46);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(584, 297);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.pictureBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(576, 271);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Paste Here";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(576, 317);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(570, 265);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnSettings);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(584, 46);
+            this.panel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(362, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Paste an image below to upload it to Azure blob storage, and get a link to it.";
+            // 
+            // btnSettings
+            // 
+            this.btnSettings.Location = new System.Drawing.Point(497, 12);
+            this.btnSettings.Name = "btnSettings";
+            this.btnSettings.Size = new System.Drawing.Size(75, 23);
+            this.btnSettings.TabIndex = 1;
+            this.btnSettings.Text = "Settings....";
+            this.btnSettings.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 343);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Image Cloud Paste";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnSettings;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
